644d849c640de40fe41e375929da37c576913a98
ncmatteo <ncmatteo@gmail.com>
2024-09-16 09:23:46 +1100

    Reponse Q1.2

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ Keep-Alive: timeout=5
 Transfer-Encoding: chunked
 
 
+Question 1.2 donner la liste des en-têtes qui ont changé depuis la version précédente.
+
+
+HTTP/1.1 200 OK
+Content-Type: application/json
+Date: Sun, 15 Sep 2024 22:21:58 GMT
+Connection: keep-alive
+Keep-Alive: timeout=5
+Content-Length: 20
+
+Il y a :
+-Content-Type: application/json
+-Content-Length: 20
+
+qui se sont rajouté
+
+
--- server-http.mjs
+++ server-http.mjs
@@ -2,12 +2,17 @@ import http from "node:http";
 
 const host = "localhost";
 const port = 8000;
-
+/*
 function requestListener(_request, response) {
   response.writeHead(200);
   response.end("<html><h1>My first server!<h1></html>");
 }
+*/
 
+function requestListener(_request, response) {
+  response.setHeader("Content-Type", "application/json");
+  response.end(JSON.stringify({ message: "I'm OK" }));
+}
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
